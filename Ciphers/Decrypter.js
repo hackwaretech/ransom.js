d5e24e7a8869631727b5782dd2667ba1477148d9
Thauan Santos <nero.rbr@gmail.com>
2019-03-26 03:28:25 -0300

    changed encryption and decryption to keys mode

--- Ciphers/Decrypter.js
+++ Ciphers/Decrypter.js
@@ -1,16 +1,12 @@
 const path = require("path");
 const Crypto = require("crypto");
 const fs = require("fs");
-const algorithm = "aes-256-ctr";
 
 // endereço do arquivo de chaves
-const keypath = path.join(__dirname, "..", "keys", "key.json");
+const keypath = path.join(__dirname, "..", "keys", "key.priv");
 // lê o arquivo e parseia os dados
-const keys = JSON.parse(fs.readFileSync(keypath));
-// transofrma as keys em valores válidos
-const KEY = Buffer.from(keys.key, "hex");
-const IV = Buffer.from(keys.iv, "hex");
+const privKey = fs.readFileSync(keypath);
 // cria uma cifra responsável por decifrar os dado
-const cipher = Crypto.createDecipheriv(algorithm, KEY, IV);
+const cipher = data => Crypto.privateDecrypt(privKey, data);
 
 module.exports = cipher;
