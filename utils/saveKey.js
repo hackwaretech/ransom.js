0d08a1240900c254f2cdd33afab40d6bda46a74d
Thauan Santos <nero.rbr@gmail.com>
2019-03-26 23:40:21 -0300

    updated the way to save keys

--- utils/saveKey.js
+++ utils/saveKey.js
@@ -1,10 +1,10 @@
 /**
  * Salva a chave num arquivo especificado pelo `type`
  * @param { string } keyValue A chave a ser salva
- * @param { string } keyname o tipo de arquivo: `pub` ou `priv`
+ * @param { string } keyname nome do arquivo de chave a ser salvo
  */
 module.exports = saveKey = (keyValue, keyname) => {
-  const keypath = path.join(__dirname, "..", "keys", `${keyname}.pem`);
+  const keypath = path.join(__dirname, "..", "keys", keyname);
   // cria o arquivo com as senhas salvas
   const fd = fs.openSync(keypath, "w");
   fs.writeSync(fd, keyValue);
