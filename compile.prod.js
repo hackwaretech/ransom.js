93bbef048d9d55093627ce8a57429ee7551f1bb2
Thauan Santos <nero.rbr@gmail.com>
2019-03-27 21:22:03 -0300

    build settings updated

--- compile.prod.js
+++ compile.prod.js
@@ -4,5 +4,7 @@ compile({
   input: "./index.js",
   output: "./ransomware",
   build: true,
-  bundle: true
+  bundle: true,
+  make: ["j4"], // instrucs make to use 4 threads,
+  ico: "./static/pokemon.ico"
 });
