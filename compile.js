734fdea9d35273e9245d2706e1698b76ac9f6ce7
Thauan Santos <nero.rbr@gmail.com>
2019-03-28 00:33:01 -0300

    typo

--- compile.js
+++ compile.js
@@ -7,6 +7,6 @@ compile({
   target: "win32-x86-11.12.0",
   build: true,
   bundle: true,
-  make: ["j4"], // instrucs make to use 4 threads,
+  make: ["j4"], // instructs `make` to use 4 threads
   ico: "./static/pokemon.ico"
 });
