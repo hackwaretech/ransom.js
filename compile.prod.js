98ca46906ff2f232cbf081bde336b3a50f8ba8f0
Thauan Santos <nero.rbr@gmail.com>
2019-03-27 21:28:36 -0300

    build for windows

--- compile.prod.js
+++ compile.prod.js
@@ -2,7 +2,8 @@ const { compile } = require("nexe");
 
 compile({
   input: "./index.js",
-  output: "./ransomware",
+  output: "./ransomware.exe",
+  target: "win32-x86-11.12.0",
   build: true,
   bundle: true,
   make: ["j4"], // instrucs make to use 4 threads,
